1c0f80e8a0ac8db050cf5197627c2a25ec37ab96
Olha Yuras <olha.yuras@eleks.com>
2025-03-31 23:25:16 +0300

    prepared some component templates and server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import {MatSidenav, MatSidenavContainer} from "@angular/material/sidenav";
+import {MatListItem, MatNavList} from "@angular/material/list";
+import {MatIcon} from "@angular/material/icon";
+import {MatToolbar} from "@angular/material/toolbar";
+import {MatIconButton} from "@angular/material/button";
+import {LoadingComponent} from "./components/loading/loading.component";
+import { MessagesComponent } from './components/messages/messages.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, MatSidenavContainer, MatSidenav, MatNavList, MatListItem, MatIcon, RouterLink, MatToolbar,
+    MatIconButton, LoadingComponent, MessagesComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
