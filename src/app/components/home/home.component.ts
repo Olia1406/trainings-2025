e7ff0db2a712c4ed6c025ab9d6fb93c80cbe38d8
Olha Yuras <olha.yuras@eleks.com>
2025-04-23 20:28:24 +0300

    changed theme, start working on creating app, created service with native fetch and promises

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
-import {MatTab, MatTabGroup} from "@angular/material/tabs";
+import { Component, inject, signal } from '@angular/core';
+import { MatTab, MatTabGroup } from "@angular/material/tabs";
 import { CoursesCardListComponent } from "../courses-card-list/courses-card-list.component";
+import { Course } from '../../models/course.model';
+import { CoursesService } from '../../services/courses.service';
+import { CoursesServiceWithFetch } from '../../services/courses-fetch.service';
 
 @Component({
   selector: 'app-home',
@@ -14,5 +17,27 @@ import { CoursesCardListComponent } from "../courses-card-list/courses-card-list
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
+  courses = signal<Course[]>([]);
+
+  coursesService = inject(CoursesServiceWithFetch);
+
+  constructor() {
+    this.loadCourses().then(() => {
+      console.log("Courses loaded", this.courses());
+    })
+  }
+
+  async loadCourses() {
+    try {
+      const courses = await this.coursesService.loadAllCourses();
+      this.courses.set(courses);
+    }
+    catch (error) {
+      console.error("Error loading courses", error);
+    }
+
+
+  }
 
 }
