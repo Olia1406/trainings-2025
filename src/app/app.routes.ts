a883a1c6799767b9a8fbc0b616eb1d1fb5d0e633
Olha Yuras <olha.yuras@eleks.com>
2025-06-24 11:51:53 +0300

    linkedSigna() and resource() signals

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,8 @@ import { CourseComponent } from './components/course/course.component';
 import { courseResolver } from './components/course/course-resolver';
 import { courseLessonsResolver } from './components/course/course-lessons-resolver';
 import { LessonsComponent } from './components/lessons/lessons.component';
+import { ResourceDemoComponent } from './components/resource-demo/resource-demo.component';
+import { LinkedSignalDemoComponent } from './components/linked-signal-demo/linked-signal-demo.component';
 
 
 export const routes: Routes = [
@@ -22,14 +24,14 @@ export const routes: Routes = [
     path: "lessons",
     component: LessonsComponent
   },
-  // {
-  //   path:"shopping-cart",
-  //   component: LinkedSignalDemoComponent
-  // },
-  // {
-  //   path: "resource-demo",
-  //   component: ResourceDemoComponent
-  // },
+  {
+    path:"shopping-cart",
+    component: LinkedSignalDemoComponent
+  },
+  {
+    path: "resource-demo",
+    component: ResourceDemoComponent
+  },
   {
     path: 'courses/:courseId',
     component: CourseComponent,
